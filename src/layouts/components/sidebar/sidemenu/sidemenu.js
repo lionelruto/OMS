195fe012d61c99aae48210d7de42b2eafe55a14e
lionel mambingo <lionelmambingo@gmail.com>
2020-10-28 03:01:48 +0000

    Update src/layouts/components/sidebar/sidemenu/sidemenu.js, src/containers/dashboards/index.js, src/components/pages/patients/patients.js, src/components/pages/modal/modal.js files Deleted src/containers/i

--- src/layouts/components/sidebar/sidemenu/sidemenu.js
+++ src/layouts/components/sidebar/sidemenu/sidemenu.js
@@ -49,19 +49,16 @@ class SideMenuContent extends Component {
       return (
          <SideMenu className="sidebar-content" toggleSidebarMenu={this.props.toggleSidebarMenu}>
             <SideMenu.MenuMultiItems
-               name="Dashboard"
+               name="DashbToard"
                Icon={<EqualizerIcon style={{fontSize:18}} />}
                ArrowRight={<ChevronRight size={16} />}
-               collapsedSidebar={this.props.collapsedSidebar}
-            >
+               collapsedSidebar={this.props.collapsedSidebar}>
                <NavLink to={DASHBOARD} exact className="item" activeclassname="active">
-             
                   <span className="menu-item-text">Statistiques</span>
                   {/* <span className="menu-item-text">Mes-Statistiques</span> */}
                </NavLink>
-               
-               
-            </SideMenu.MenuMultiItems>
+           </SideMenu.MenuMultiItems>
+            
             <SideMenu.MenuSingleItem>
                <NavLink to={LIST_USER_ROUTE} >
                   <i className="menu-icon">
@@ -70,6 +67,7 @@ class SideMenuContent extends Component {
                   <span className="menu-item-text">Users</span>
                </NavLink>
             </SideMenu.MenuSingleItem>
+
             <SideMenu.MenuSingleItem>
                <NavLink to={LIST_PATIENT_ROUTE} >
                   <i className="menu-icon">
@@ -95,6 +93,7 @@ class SideMenuContent extends Component {
                   <span className="menu-item-text">Cartes</span>
                </NavLink>
             </SideMenu.MenuSingleItem>
+
             <SideMenu.MenuSingleItem >
                <NavLink to={LIST_FILE_ROUTE} >
                   <i className="menu-icon">
@@ -103,13 +102,14 @@ class SideMenuContent extends Component {
                   <span className="menu-item-text">Pre-Consultaion</span>
                </NavLink>
             </SideMenu.MenuSingleItem>
+
             <SideMenu.MenuSingleItem >
                <NavLink   to={LIST_CONSULTATION_ROUTE}>
                {/* to={LIST_CONSULTATION_ROUTE} */}
                   <i className="menu-icon">
                      <FaStethoscope size={18} color="#fff" />
                   </i>
-                  <span className="menu-item-text">Consultaion</span>
+                  <span className="menu-item-text">Consultation</span>
                </NavLink>
             </SideMenu.MenuSingleItem>
          
